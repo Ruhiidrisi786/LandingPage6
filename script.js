77220aa471964a6837ce89e07d24633aea676168
Rukaiya Bano <rukaiyabano888@gmail.com>
2024-02-16 06:25:46 -0800

    complete

---
+++ script.js
@@ -0,0 +1,19 @@
+gsap.from("#main .img1", {
+    y:60,
+    duration:1,
+    delay: 0.5,
+    opacity:0
+})
+gsap.from("#main .img2", {
+    x:60,
+    duration:1,
+    delay: 0.5,
+    opacity:0
+})
+
+gsap.from("#main .img3", {
+    y: -60,
+    duration:1,
+    delay:0.5,
+    opacity:0,
+})
